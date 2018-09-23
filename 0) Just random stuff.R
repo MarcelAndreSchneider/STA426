39befb8967b99f06993d8347bc162cbb933d473b
MarcelAndreSchneider <marcelandre.schneider@gmail.com>
2018-09-23 16:31:45 +0200

    new
    whateva

---
+++ 0) Just random stuff.R
@@ -0,0 +1,28 @@
+
+
+
+??text.r
+?sample
+
+sample
+
+
+sample(1:5, 10, replace=T)
+
+
+
+set.seed(42)
+groups <- rep(1:3, each = 10)
+measure <- c(5, 1, 5)[groups] + rnorm(30)
+boxplot(measure ~ groups)
+summary(aov(measure ~ groups))
+
+
+
+
+
+
+
+
+
+
